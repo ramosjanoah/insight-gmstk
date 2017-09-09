3b3c6fce7a3036d81d5d0d075de02dade45d901e
joshuakosasih <joshua.kosasih@gmail.com>
2017-09-09 21:10:41 +0700

    update datas

--- data/ramos-3/tfidf.py
+++ data/ramos-3/tfidf.py
@@ -20,18 +20,6 @@ tfidf_matrix = tfidf.fit_transform(corpus)
 feature_names = tfidf.get_feature_names()
 dense = tfidf_matrix.todense()
 
-for idx in range(0, len(file_list)-1):
-    d = dense[idx].tolist()[0]
-    pair = []
-    for idx, val in enumerate(d):
-        if val > 0:
-            pair.append((val, idx))
-
-    spair = sorted(pair, reverse=True)
-    stopic = []
-    for (val, idx) in spair[:5]:
-        stopic.append(feature_names[idx])
-
 
 data = {}
 for i in range(0, len(file_list)-1):
